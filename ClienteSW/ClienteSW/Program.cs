87d5db22f07a2f516e66c6b433c02f6e6ab3c213
LChumi <112025391+LChumi@users.noreply.github.com>
2022-12-13 22:31:35 -0500

    Add files via upload

---
+++ ClienteSW/ClienteSW/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClienteSW
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ServicioWebNet.WSOperacionesClient cliente= new ServicioWebNet.WSOperacionesClient();
+            // metodos 
+            //loguin
+            if (cliente.loguin("LChumi", "chumi123"))
+            {
+                Console.WriteLine("Acceso correcto");
+            }
+            else
+            {
+                Console.WriteLine("Acceso denegado");
+            }
+            //cerrar secion 
+            cliente.Close();
+            Console.ReadKey();
+        }
+    }
+}
